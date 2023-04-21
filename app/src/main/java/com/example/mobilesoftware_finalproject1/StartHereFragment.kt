c7a86e182502c0d6bed91deb744961d28e47bd50
emilymack36 <emilymack36@gmail.xom>
2023-04-21 15:03:49 -0400

    updated fragment_first.xml and fragment_first.xml, added fragment_start_here.xml and StartHereFragment.kt

---
+++ app/src/main/java/com/example/mobilesoftware_finalproject1/StartHereFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mobilesoftware_finalproject1
+
+import androidx.fragment.app.Fragment
+import com.example.mobilesoftware_finalproject1.databinding.FragmentStartHereBinding
+
+class StartHereFragment: Fragment() {
+    private var _binding: FragmentStartHereBinding? = null
+
+}
