a31461602b9a7ba99b99f2a0211cb5a2e1bca2d9
emilymack36 <emilymack36@gmail.xom>
2023-04-23 19:12:30 -0400

    welcome and labreportinstructions work

--- app/src/main/java/com/example/mobilesoftware_finalproject1/StartHereItemsFragment.kt
+++ app/src/main/java/com/example/mobilesoftware_finalproject1/StartHereItemsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mobilesoftware_finalproject1
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,16 +19,23 @@ class StartHereItemsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentStartHereItemsBinding.inflate(inflater, container, false)
+        val args = arguments ?: return binding.root
+        val textResourceId = args.getInt("textResourceId")
+        val text = getString(textResourceId)
+
+        binding.textView.text = text
+
+        Log.d("StartHereItemsFragment", "text: $text") // add this log statement to check the value of text
+
+
         return binding.root
-
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
