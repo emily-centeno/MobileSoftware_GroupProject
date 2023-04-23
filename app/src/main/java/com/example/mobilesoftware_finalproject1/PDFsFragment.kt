dd8f6c9770446af72bb5e9a76489d91ed0b3ea89
emilymack36 <emilymack36@gmail.xom>
2023-04-23 19:30:21 -0400

    welcome and labreportinstructions work

--- app/src/main/java/com/example/mobilesoftware_finalproject1/PDFsFragment.kt
+++ app/src/main/java/com/example/mobilesoftware_finalproject1/PDFsFragment.kt
@@ -1,19 +1,18 @@
 package com.example.mobilesoftware_finalproject1
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.mobilesoftware_finalproject1.databinding.FragmentStartHereItemsBinding
+import com.example.mobilesoftware_finalproject1.databinding.FragmentPdfsBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class StartHereItemsFragment : Fragment() {
+class PDFsFragment : Fragment() {
 
-    private var _binding: FragmentStartHereItemsBinding? = null
+    private var _binding: FragmentPdfsBinding   ? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,16 +23,7 @@ class StartHereItemsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentStartHereItemsBinding.inflate(inflater, container, false)
-        val args = arguments ?: return binding.root
-        val textResourceId = args.getInt("textResourceId")
-        val text = getString(textResourceId)
-
-        println(textResourceId)
-        binding.textView.text = text
-
-        Log.d("StartHereItemsFragment", "text: $text") // add this log statement to check the value of text
-
+        _binding = FragmentPdfsBinding.inflate(inflater, container, false)
 
         return binding.root
     }
--- app/src/main/java/com/example/mobilesoftware_finalproject1/SyllabusFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mobilesoftware_finalproject1
-
-class SyllabusFragment {
-}
