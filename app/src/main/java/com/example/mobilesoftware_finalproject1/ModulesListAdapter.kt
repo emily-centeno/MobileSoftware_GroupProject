119bdc00615bcd8a134d09e2e0567e79d85663ff
emilymack36 <emilymack36@gmail.xom>
2023-04-23 23:21:36 -0400

    learning modules

--- app/src/main/java/com/example/mobilesoftware_finalproject1/ModulesListAdapter.kt
+++ app/src/main/java/com/example/mobilesoftware_finalproject1/ModulesListAdapter.kt
@@ -8,35 +8,22 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class ModulesListAdapter(private val itemList: List<Item>) : RecyclerView.Adapter<ModulesListAdapter.ViewHolder>() {
+class ModulesListAdapter(private val dataList: List<String>) :
+    RecyclerView.Adapter<ModulesListViewHolder>() {
 
-    // ViewHolder class with references to the views in the item layout
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.item_text_view)
-        val button: Button = itemView.findViewById(R.id.item_button)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModulesListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_view_item, parent, false)
+        return ModulesListViewHolder(view)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
-        return ViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentItem = itemList[position]
-
-        // Update the TextView and Button with the values from the current item
-        holder.textView.text = currentItem.text
-        holder.button.text = currentItem.buttonText
-
-        // Set an OnClickListener for the Button
+    override fun onBindViewHolder(holder: ModulesListViewHolder, position: Int) {
+        val item = dataList[position]
+        holder.textView.text = item
         holder.button.setOnClickListener {
-            // Do something when the Button is clicked
+            // Handle button click
         }
     }
 
-    override fun getItemCount() = itemList.size
-
-    // Data class representing a single item in the list
-    data class Item(val text: String, val buttonText: String)
-
+    override fun getItemCount() = dataList.size
 }
