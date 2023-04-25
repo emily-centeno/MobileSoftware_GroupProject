8c69c9b86682d23c1454cff236987095e191d510
emilymack36 <emilymack36@gmail.xom>
2023-04-24 22:53:03 -0400

    fix commit

--- app/src/main/java/com/example/mobilesoftware_finalproject1/SyllabusFragment.kt
+++ app/src/main/java/com/example/mobilesoftware_finalproject1/SyllabusFragment.kt
@@ -1,32 +1,36 @@
 package com.example.mobilesoftware_finalproject1
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.mobilesoftware_finalproject1.databinding.FragmentPdfsBinding
+import androidx.navigation.fragment.findNavController
+import com.example.mobilesoftware_finalproject1.databinding.FragmentSyllabusBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class PDFsFragment : Fragment() {
+class SyllabusFragment : Fragment() {
 
-    private var _binding: FragmentPdfsBinding   ? = null
+    private var _binding: FragmentSyllabusBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPdfsBinding.inflate(inflater, container, false)
 
+        _binding = FragmentSyllabusBinding.inflate(inflater, container, false)
         return binding.root
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
